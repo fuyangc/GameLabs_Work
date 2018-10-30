7038a12904c32f633e52beb9eafafa6d7cdb8637
Fu Yang Chang <chanf031@newschool.edu>
2018-10-30 15:07:30 -0400

    card game test

--- Card Game/Assets/Drag/DropZone.cs
+++ Card Game/Assets/Drag/DropZone.cs
@@ -3,25 +3,45 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.EventSystems;
 
-public class DropZone : MonoBehaviour, IDropHandler {
+public class DropZone : MonoBehaviour, IDropHandler
+{
 
-    public void OnPointerEnter(PointerEventData eventData){
+    
+
+
+    public void OnPointerEnter(PointerEventData eventData)
+    {
 
 
     }
 
-    public void OnpointerExit(PointerEventData eventData){
+    public void OnpointerExit(PointerEventData eventData)
+    {
 
 
     }
 
-    public void OnDrop(PointerEventData eventData){
+    public void OnDrop(PointerEventData eventData)
+    {
         Drag d = eventData.pointerDrag.GetComponent<Drag>();
-        if(d !=null){
+        if (d != null)
+        {
             d.parentToReturnTo = this.transform;
 
         }
 
 
     }
+
+    public void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.tag == "card1")
+        {
+            Debug.Log("card 1 present");
+        
+        }
+    }
+
 }
+    
+
